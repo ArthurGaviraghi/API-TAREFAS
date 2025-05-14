635d8ec529959ec5ca6ef42a809d21894b7753eb
Arthur Gaviraghi <arthur_gaviraghi@estudante.sesisenai.org.br>
2025-05-14 19:40:19 -0300

    enviado

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express'
+// Permitir todas as origens (mais comum em dev)
+import cors from 'cors';
+
+// importar o dotenv
+import 'dotenv/config'
+import { router } from './rotas/usuario.js'
+import { routerTarefa } from './rotas/tarefas.js'
+import { database } from './database.js'
+
+const app = express()
+app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(express.json())
+app.use(router)
+app.use(routerTarefa)
+
+await database.sync()
+
+app.listen(5000, () => console.log('servidor rodando'))
